6e7f9cfd627b42120f7dcea3a38c030de90ae315
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-03-04 22:42:27 +0500

    Structure ban gya hai

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,7 @@
-import "./page.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./page.css";
+import { faPenToSquare, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 
 
 export default function Home() {
@@ -6,15 +9,30 @@ export default function Home() {
     <div className="body">
       <div className="card">
         <div className="inputdiv">
-          <input type="text" className="inputOfToDoList" />
+          <input type="text" className="inputOfToDoList" placeholder="eg: Wake up at 08:00 AM" />
+          <div className="searhBarDiv">
+            <FontAwesomeIcon icon={faPlus} width={20} />
+          </div>
         </div>
         <div className="listDiv">
           <div className="singleDiv">
-            <div className="mainMessage"></div>
+            <div className="mainMessage">Hy to everyone</div>
             <div className="btnDiv">
-            
+              <FontAwesomeIcon className="iconsOfEdit" icon={faPenToSquare} width={20} />
+              <FontAwesomeIcon className="iconsOfEdit" icon={faTrash} width={20} />
             </div>
           </div>
+          <div className="singleDiv">
+            <div className="mainMessage">Hy to everyone</div>
+            <div className="btnDiv">
+              <FontAwesomeIcon className="iconsOfEdit" icon={faPenToSquare} width={20} />
+              <FontAwesomeIcon className="iconsOfEdit" icon={faTrash} width={20} />
+            </div>
+          </div>
+        </div>
+
+        <div className="deleteListDiv">
+          <button>Delete All Todos</button>
         </div>
       </div>
     </div>
